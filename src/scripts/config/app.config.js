7d1d55e1b15176ef7afeeb8cdd6a0f467d406183
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-25 02:16:52 +0700

    config(main): add supabase url and key

--- src/scripts/config/app.config.js
+++ src/scripts/config/app.config.js
@@ -6,6 +6,10 @@ const config = {
     version: 1,
     objectStorageName: 'aljaar-db',
   },
+  supabase: {
+    url: 'https://iifjmhhbusjlypxbpniy.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpZmptaGhidXNqbHlweGJwbml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4NzAwMjgsImV4cCI6MTk4MzQ0NjAyOH0.epXBWgRKGVXrVXZDAoeMPcQ4ocAKaNA0jvUzBUoNzqQ'
+  }
 };
 
 export default config;
