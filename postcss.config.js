ce146fc57e338d74704709a2720dd5560bdace12
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-25 02:12:42 +0700

    config(postcss): add postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: [
-    'postcss-preset-env',
-    'tailwindcss'
-  ],
-};
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    'postcss-preset-env': {},
+  }
+}
