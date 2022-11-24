3b52b2388c3c2458051b76486a1b2467d6737db3
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-25 02:16:31 +0700

    component(new): navigation-navbar

--- src/scripts/components/navigation-navbar.js
+++ src/scripts/components/navigation-navbar.js
@@ -1,15 +1,99 @@
+import UrlParser from "../utils/url.parser";
+
 class NavigationNavbar extends HTMLElement {
   constructor () {
+    super();
+
     this.states = [
-      { id: 'home', route: '/',  }
+      { id: 'home', route: '/', },
+      { id: 'maps', route: '/maps', },
+      { id: 'notif', route: '/notification', },
+      { id: 'share', route: '/product', },
+      { id: 'profile', route: '/profile', },
     ]
+
+    this.render();
+    this.afterRender();
   }
 
-  set activeState (state) {
+  get activeState () {
+    const activeRoute = UrlParser.parseActiveUrlWithCombiner();
 
+    return this.states.find(item => item.route === activeRoute) ?? {
+      id: 'home', route: '/'
+    };
   }
 
+  onMenuItemClicked (element) {
+    const route = this.states.find(item => item.id === element.id.replace('navbar-', ''));
+    console.log(route)
+    if (!route) {
+      window.location.href = '/#/';
+    } else {
+      window.location.href = `/#${route.route}`;
+    }
 
+    this.initializeActiveClassState();
+  }
+
+  render () {
+    this.innerHTML = String.raw`
+      <div id="navbar-home" class="menu-item">
+        <div class="menu-item--icon">
+          <iconify-icon icon="ri:home-4-line"></iconify-icon>
+        </div>
+        <span class="menu-item--text inline-block">Home</span>
+      </div>
+      <div id="navbar-maps" class="menu-item">
+        <div class="menu-item--icon">
+          <iconify-icon icon="ri:road-map-line"></iconify-icon>
+        </div>
+        <span class="menu-item--text inline-block">Maps</span>
+      </div>
+      <div id="navbar-share" class="menu-item">
+        <div class="menu-item--icon">
+          <iconify-icon icon="ri:add-circle-fill"></iconify-icon>
+        </div>
+        <span class="menu-item--text inline-block">Share</span>
+      </div>
+      <div id="navbar-notif" class="menu-item">
+        <div class="menu-item--icon">
+          <iconify-icon icon="ri:notification-4-line"></iconify-icon>
+        </div>
+        <span class="menu-item--text inline-block">Notification</span>
+      </div>
+      <div id="navbar-profile" class="menu-item">
+        <div class="menu-item--icon">
+          <iconify-icon icon="ri:user-line"></iconify-icon>
+        </div>
+        <span class="menu-item--text inline-block">Profile</span>
+      </div>
+    `;
+  }
+
+  afterRender () {
+    const items = document.querySelectorAll('.menu-item');
+
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        this.onMenuItemClicked(item);
+      })
+    });
+
+    this.initializeActiveClassState();
+  }
+
+  initializeActiveClassState () {
+    const items = document.querySelectorAll('.menu-item');
+
+    items.forEach(item => {
+      item.classList.remove('active');
+
+      if (item.id.includes(this.activeState.id)) {
+        item.classList.add('active');
+      }
+    })
+  }
 }
 
 export default NavigationNavbar;
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -24,6 +24,7 @@ class App {
   }
 
   _initialCustomElement() {
+    customElements.define('navigation-navbar', NavigationNavbar);
   }
 
   async renderPage() {
