3d145fcc2d2f4700fd8f132a61ba8f45775c9464
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-25 02:14:17 +0700

    routes(new): add page route

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,5 +1,20 @@
+import HomeView from '../views/pages/home.view';
+
 const routes = {
-  '/': import(/* webpackChunkName: "home-view" */ '../views/pages/home.view')
+  '/': HomeView,
+  '/signin': import(/* webpackChunkName: "signin-view" */ '../views/pages/auth/signin.view')
+    .then((view) => view.default),
+  '/signup': import(/* webpackChunkName: "signup-view" */ '../views/pages/auth/signup.view')
+    .then((view) => view.default),
+  '/maps': import(/* webpackChunkName: "maps-view" */ '../views/pages/maps.view')
+    .then((view) => view.default),
+  '/notification': import(/* webpackChunkName: "notification-view" */ '../views/pages/notification.view')
+    .then((view) => view.default),
+  '/profile': import(/* webpackChunkName: "profile-view" */ '../views/pages/profile.view')
+    .then((view) => view.default),
+  '/product': import(/* webpackChunkName: "new-product-view" */ '../views/pages/product/new.view')
+    .then((view) => view.default),
+  '/product/:id': import(/* webpackChunkName: "product-detail-view" */ '../views/pages/product/detail.view')
     .then((view) => view.default),
 };
 
