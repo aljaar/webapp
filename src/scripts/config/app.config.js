bca033ccda08bb358e2761eb905d4356c2807ffb
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-28 01:24:37 +0700

    chore(mapbox): add mapbox token to config
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>
    Co-Authored-By: iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>

--- src/scripts/config/app.config.js
+++ src/scripts/config/app.config.js
@@ -9,7 +9,10 @@ const config = {
   supabase: {
     url: 'https://iifjmhhbusjlypxbpniy.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpZmptaGhidXNqbHlweGJwbml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4NzAwMjgsImV4cCI6MTk4MzQ0NjAyOH0.epXBWgRKGVXrVXZDAoeMPcQ4ocAKaNA0jvUzBUoNzqQ'
-  }
+  },
+  mapbox: {
+    token: 'pk.eyJ1IjoicnlhbmF1bnVyIiwiYSI6ImNrN3FhdWlzNzAxbDEzZm52bHZwZm8yNGwifQ.h60r6ue0FMfXHfpiF96tZg',
+  },
 };
 
 export default config;
