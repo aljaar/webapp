5a4a0166b00ca292bdfe851292abcbab9d5bb6bb
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-27 12:09:12 +0700

    view(home): add filter feature
    Need integrate with javascript soon

--- src/scripts/components/alpine/products.js
+++ src/scripts/components/alpine/products.js
@@ -4,7 +4,9 @@ export default () => ({
   items: [],
   itemsAll: [],
   filter: {
-    open: false
+    open: false,
+    filters: new Map(),
+    category: null
   },
   async init() {
     // const { data: products } = await service.lists.near(200);
@@ -71,5 +73,35 @@ export default () => ({
   image (item) {
     const { data: { publicUrl } } = service.helpers.usePublicUrl(item);
     return publicUrl;
+  },
+  toggleFilter(column, type) {
+    this.filter.filters.set('active', column);
+    this.filter.filters.set('sort', type);
+  },
+  toggleCategory(type) {
+    if (this.filter.category !== type) {
+      this.filter.category = type;
+    } else {
+      this.filter.category = null;
+    }
+
+    console.log(this.filter.category)
+  },
+  isFilterActive(column, type) {
+    if (!this.filter.filters.has('active')) return false;
+
+    return (this.filter.filters.get('active') === column && this.filter.filters.get('sort') === type);
+  },
+  useFilterClass(column, type) {
+    return {
+      'text-white bg-emerald-500': this.isFilterActive(column, type), 
+      'text-emerald-700 bg-emerald-50 ring-1 ring-emerald-500': !this.isFilterActive(column, type)
+    }
+  },
+  useFilterCategoryClass(type) {
+    return {
+      'text-white bg-emerald-600': (this.filter.category === type), 
+      'text-emerald-700 bg-emerald-50 ring-1 ring-emerald-500': (this.filter.category !== type)
+    }
   }
 });
--- src/scripts/views/pages/home.view.js
+++ src/scripts/views/pages/home.view.js
@@ -5,7 +5,7 @@ const HomeView = {
     return String.raw`
       <button id="login" class="hidden">Login</button>
 
-      <div x-data="products" class="flex flex-col px-4 pt-4 gap-4">
+      <div x-data="products" class="flex flex-col px-4 pt-4 gap-3">
         <!-- Search -->
         <form>   
           <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
@@ -18,22 +18,22 @@ const HomeView = {
         </form>
 
         <!-- Filter -->
-        <div class="flex gap-3">
+        <div class="flex gap-2">
           <button @click="filter.open = !filter.open" type="button" class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
             <iconify-icon icon="ri:filter-3-fill"></iconify-icon>
           </button>
-          <button class="text-white bottom-2.5 bg-gray-500 hover:bg-gray-800 focus:ring-2 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
+          <button @click="toggleCategory('food')" x-bind:class="useFilterCategoryClass('food')" class="rounded-lg text-sm px-3 pt-2 pb-1">
             Food
           </button>
-          <button class="text-white bottom-2.5 bg-gray-500 hover:bg-gray-800 focus:ring-2 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
+          <button @click="toggleCategory('non-food')" x-bind:class="useFilterCategoryClass('non-food')" class="rounded-lg text-sm px-3 pt-2 pb-1">
             Non-Food
           </button>
         </div>
 
         <!-- Product Lists -->
-        <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-2 divide-y">
           <template x-for="item in items">
-            <div class="flex gap-4">
+            <div class="flex pt-2 pb-1 gap-4">
               <div class="">
                 <img class="lazyload w-32 h-24 object-cover rounded" x-bind:data-src="image(item.image)" alt="">
               </div>
@@ -65,36 +65,43 @@ const HomeView = {
         </div>
 
         <div class="sheet-modal bg-white border-t p-4 border-t-gray-200 " x-bind:class="{'active': filter.open}">
+          <h4 class="text-emerald-500 mb-3">Filter</h4>
+
           <div class="flex flex-col gap-2 divide-y">
-              <div class="pt-2 flex justify-between">
-                <h4 class="">Berdasarkan X</h4>
+              <div class="pt-2 flex items-center justify-between">
+                <h4 class="">Jarak</h4>
                 <div class="flex gap-2">
-                  <button class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
+                  <button 
+                    @click="toggleFilter('distance', 'asc')"
+                    x-bind:class="useFilterClass('distance', 'asc')" 
+                    class="bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-3 pt-2 pb-1"
+                  >
                     <iconify-icon icon="ri:sort-asc" inline></iconify-icon>
                   </button>
-                  <button class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
+                  <button 
+                    @click="toggleFilter('distance', 'desc')"
+                    x-bind:class="useFilterClass('distance', 'desc')" 
+                    class="bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-3 pt-2 pb-1"
+                  >
                     <iconify-icon icon="ri:sort-desc" inline></iconify-icon>
                   </button>
                 </div>
               </div>
-              <div class="pt-2 flex justify-between">
-                <h4 class="">Berdasarkan X</h4>
+              <div class="pt-2 flex items-center justify-between">
+                <h4 class="">View</h4>
                 <div class="flex gap-2">
-                  <button class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
+                  <button 
+                    @click="toggleFilter('view', 'asc')"
+                    x-bind:class="useFilterClass('view', 'asc')" 
+                    class="bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-3 pt-2 pb-1"
+                  >
                     <iconify-icon icon="ri:sort-asc" inline></iconify-icon>
                   </button>
-                  <button class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
-                    <iconify-icon icon="ri:sort-desc" inline></iconify-icon>
-                  </button>
-                </div>
-              </div>
-              <div class="pt-2 flex justify-between">
-                <h4 class="">Berdasarkan X</h4>
-                <div class="flex gap-2">
-                  <button class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
-                    <iconify-icon icon="ri:sort-asc" inline></iconify-icon>
-                  </button>
-                  <button class="text-white bottom-2.5 bg-emerald-500 hover:bg-emerald-800 focus:ring-2 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3 pt-2 pb-1">
+                  <button 
+                    @click="toggleFilter('view', 'desc')"
+                    x-bind:class="useFilterClass('view', 'desc')" 
+                    class="bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-3 pt-2 pb-1"
+                  >
                     <iconify-icon icon="ri:sort-desc" inline></iconify-icon>
                   </button>
                 </div>
