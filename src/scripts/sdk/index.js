a44784017d29506806376f4ef2225af8ab9eebba
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-28 02:20:13 +0700

    chore(eslint): fix eslint
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>
    Co-Authored-By: iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>

--- src/scripts/sdk/index.js
+++ src/scripts/sdk/index.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 import config from '../config/app.config';
-import useSDK from "./sdk";
+import useSDK from './sdk';
 
 const supabase = createClient(config.supabase.url, config.supabase.key);
 
