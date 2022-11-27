939be80418ec75d117085e2c4c128946a1ae821a
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-27 23:29:13 +0700

    view(profile-edit): initialize view
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>

---
+++ src/scripts/views/pages/profile.edit.view.js
@@ -0,0 +1,13 @@
+class ProfileEditView {
+  async render () {
+    return String.raw`
+      <h1>Profile Edit</h1>
+    `
+  }
+
+  async afterRender () {
+
+  }
+}
+
+export default new ProfileEditView();
