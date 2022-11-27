36fc6b3f0174a5611412a252b3e023d171c02804
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-28 01:25:35 +0700

    view(maps): init maps basic implementation
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>
    Co-Authored-By: iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>

--- src/scripts/views/pages/maps.view.js
+++ src/scripts/views/pages/maps.view.js
@@ -1,12 +1,38 @@
+import mapboxgl from 'mapbox-gl';
+import config from '../../config/app.config';
+
 class MapsView {
-  async render () {
+  async render() {
     return String.raw`
-      <h1>Maps</h1>
-    `
+      <div class="relative w-full h-full">
+        <div id="map" class="w-full h-full"></div>
+
+        <div class="absolute top-3 right-3 flex flex-col gap-3">
+          <button class="rounded-full w-12 h-12 bg-white shadow-mdl">
+            <iconify-icon icon="mdi:compass" class="text-xl" inline></iconify-icon>
+          </button>
+
+          <button class="rounded-full p-3 bg-white shadow-mdl">
+            <iconify-icon icon="mdi:home" class="text-xl" inline></iconify-icon>
+          </button>
+        </div>
+      </div>
+    `;
   }
 
-  async afterRender () {
+  async afterRender() {
+    mapboxgl.accessToken = config.mapbox.token;
 
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [112.7509655, -7.2564168],
+      zoom: 13,
+    });
+
+    map.on('load', () => {
+      console.log('map loaded');
+    });
   }
 }
 
