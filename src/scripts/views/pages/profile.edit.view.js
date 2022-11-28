5ceac60d0adf3423661a1ccbb13b3e45f429880f
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-28 21:42:08 +0700

    view(product-edit): initialize product edit view
    Co-Authored-By: iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>

--- src/scripts/views/pages/profile.edit.view.js
+++ src/scripts/views/pages/profile.edit.view.js
@@ -1,12 +1,53 @@
 class ProfileEditView {
-  async render () {
+  async render() {
     return String.raw`
-      <h1>Profile Edit</h1>
+      <!-- <h1>Profile Edit</h1> -->
+        <div>
+          <div class="p-3  top-48">
+              <div class="foto pb-3">
+                <img src="https://iifjmhhbusjlypxbpniy.supabase.co/storage/v1/object/public/avatars/public/avatar.default.webp" class="mx-auto w-24 rounded-full" alt="Profile Picture">
+              </div>
+              <div class="text-xs mx-auto pb-9 text-center">
+                <a href="" class="mx-auto text-center left-50">Change Photo</a>
+              </div>
+
+              <div class="flex flex-col gap-3">
+              <div>
+                <label for="full-name" class="text-base text-emerald-600 mb-2">Full Name</label>
+                <input type="text" id="full-name"
+                      class="w-full p-2 border rounded-md focus:outline-none focus:ring-slate-900 focus:ring-1 focus:border-neutral-900">
+              </div>
+              <div>
+                <label for="about" class="text-base text-emerald-600 mb-2">About</label>
+                <input type="text" id="about"
+                      class="w-full p-6 border rounded-md focus:outline-none focus:ring-slate-900 focus:ring-1 focus:border-neutral-900">
+              </div>
+
+              <div>
+                  <label for="email" class="text-base text-emerald-600">Email</label>
+                  <input type="text" id="tag"
+                      class="w-full p-2 border rounded-md focus:outline-none focus:ring-slate-900 focus:ring-1 focus:border-neutral-900">
+              </div>
+              <div>
+                  <label for="phone" class="text-base text-emerald-600">Phone</label>
+                  <input type="text" id="tag"
+                      class="w-full p-2 border rounded-md focus:outline-none focus:ring-slate-900 focus:ring-1 focus:border-neutral-900">
+              </div>
+
+              <br>
+              <div class="flex justify-center">
+                <button class="mx-auto bg-centerborder p-3 bg-emerald-600 pt-3 hover:bg-emerald-700 rounded-full">
+                    <h1 class="mr-3 ml-3 text-white mx-auto">Save Changes</h1>
+                </button>
+              </div>
+              </div>
+              
+          </div>
+        </div>
     `
   }
 
-  async afterRender () {
-
+  async afterRender(alpine) {
   }
 }
 
