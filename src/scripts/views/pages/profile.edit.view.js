5878496b161e76bf7d9174c10824563fdff7377c
Muhammad Khairul Anwar <khairulanwar24@users.noreply.github.com>
2022-11-30 01:22:34 +0700

    view(profile): add helper text

--- src/scripts/views/pages/profile.edit.view.js
+++ src/scripts/views/pages/profile.edit.view.js
@@ -37,6 +37,7 @@ class ProfileEditView {
             <label for="phone" class="form-control-label">Phone</label>
             <input type="text" id="tag" class="form-control">
           </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et libero eligendi atque deserunt facilis quos praesentium reprehenderit consequatur eum mollitia temporibus, quidem doloribus ipsam labore? Adipisci deleniti sequi fugit vel!</p>
           <div class="flex justify-center my-6">
             <button class="w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-emerald-600 text-white hover:bg-emerald-700">Simpan Perubahan</button>
           </div>
