5222f7aaf7b5494c36d9e3b6623f0e1657927556
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-30 01:06:05 +0700

    feat: add skeleton loading for lists
    Co-Authored-By: iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>

--- src/scripts/views/templates/creator.template.js
+++ src/scripts/views/templates/creator.template.js
@@ -13,3 +13,43 @@ export const createPageHeader = ({ title }) => String.raw`
   </div>
 `;
 
+export const listsSkeletonLoading = () => String.raw`
+  <div role="status" class="p-4 space-y-4 max-w-md rounded border border-gray-200 divide-y divide-gray-200 shadow animate-pulse">
+    <div class="flex justify-between items-center">
+      <div>
+        <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+        <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+      </div>
+      <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+    </div>
+    <div class="flex justify-between items-center pt-4">
+      <div>
+        <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+        <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+      </div>
+      <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+    </div>
+    <div class="flex justify-between items-center pt-4">
+      <div>
+        <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+        <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+      </div>
+      <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+    </div>
+    <div class="flex justify-between items-center pt-4">
+      <div>
+        <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+        <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+      </div>
+      <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+    </div>
+    <div class="flex justify-between items-center pt-4">
+      <div>
+        <div class="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+        <div class="w-32 h-2 bg-gray-200 rounded-full"></div>
+      </div>
+      <div class="h-2.5 bg-gray-300 rounded-full w-12"></div>
+    </div>
+    <span class="sr-only">Loading...</span>
+  </div>
+`;
