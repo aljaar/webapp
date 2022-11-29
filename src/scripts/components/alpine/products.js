4012c176f90fe6e768427862824ad7e875676139
Ryan Aunur <ryandevstudio@gmail.com>
2022-11-30 01:03:15 +0700

    view(home): use data from service, add loading
    Co-Authored-By: iqbalpanduwinata19 <110536862+iqbalpanduwinata19@users.noreply.github.com>
    Co-Authored-By: Muhammad Khairul Anwar <88810453+khairulanwar24@users.noreply.github.com>

--- src/scripts/components/alpine/products.js
+++ src/scripts/components/alpine/products.js
@@ -1,76 +1,27 @@
 import { service } from '../../sdk';
+import { delay } from '../../utils/helpers';
 
 export default () => ({
   items: [],
   itemsAll: [],
+  isLoading: true,
   filter: {
     open: false,
     filters: new Map(),
-    category: null
+    category: null,
   },
   async init() {
-    // const { data: products } = await service.lists.near(200);
+    const { data: products } = await service.lists.near(200);
     // console.log(products)
-    let items = [
-      {
-        product_id: 14,
-        title: 'Small Fresh Cheese',
-        description:
-          'The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive',
-        qty: 3,
-        profile: {
-          full_name: 'Ryan Aunur Rassyid',
-          avatar_url:
-            'https://lh3.googleusercontent.com/a/ALm5wu3jNDyOO1klRc5hvaS1ol48tRiiumEFhW9zZBwrvjk=s96-c',
-          rating: 0,
-        },
-        like: 2,
-        tags: ['Halal', 'Non Halal'],
-        image:
-          '09a93ad3-1313-4198-a0dd-3082a68f9fd8/e2a27baa-9377-43e4-ab34-de29dc25c0ec.png',
-        lat: -7.2496,
-        lon: 112.7417,
-        distance: 0.0012165525060606,
-      },
-      {
-        product_id: 15,
-        title: 'Tasty Concrete Chair',
-        description:
-          'New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016',
-        qty: 8,
-        profile: {
-          full_name: 'Ryan Aunur Rassyid',
-          avatar_url:
-            'https://lh3.googleusercontent.com/a/ALm5wu3jNDyOO1klRc5hvaS1ol48tRiiumEFhW9zZBwrvjk=s96-c',
-          rating: 0,
-        },
-        like: 0,
-        tags: ['Halal', 'Non Halal'],
-        image:
-          '09a93ad3-1313-4198-a0dd-3082a68f9fd8/d3e3a76c-f179-44a5-aeb5-35ec68fd8cec.jpg',
-        lat: -8.0109,
-        lon: 113.3483,
-        distance: 0.974231599774915,
-      },
-    ];
 
-    items = ([
-      ...items,
-      ...items,
-      ...items,
-      ...items,
-      ...items
-    ]).map(item => ({
-      ...item,
-      product_id: (Math.random() * 100).toFixed(0) * 1
-    }));
+    await delay(500);
 
-    this.items = items;
-    this.itemsAll = Object.freeze(items);
-
+    this.items = products;
+    this.itemsAll = Object.freeze(products);
+    this.isLoading = false;
     console.log(this.items);
   },
-  image (item) {
+  image(item) {
     const { data: { publicUrl } } = service.helpers.usePublicUrl(item);
     return publicUrl;
   },
@@ -85,7 +36,7 @@ export default () => ({
       this.filter.category = null;
     }
 
-    console.log(this.filter.category)
+    console.log(this.filter.category);
   },
   isFilterActive(column, type) {
     if (!this.filter.filters.has('active')) return false;
@@ -94,14 +45,14 @@ export default () => ({
   },
   useFilterClass(column, type) {
     return {
-      'text-white bg-emerald-500': this.isFilterActive(column, type), 
-      'text-emerald-700 bg-emerald-50 ring-1 ring-emerald-500': !this.isFilterActive(column, type)
-    }
+      'text-white bg-emerald-500': this.isFilterActive(column, type),
+      'text-emerald-700 bg-emerald-50 ring-1 ring-emerald-500': !this.isFilterActive(column, type),
+    };
   },
   useFilterCategoryClass(type) {
     return {
-      'text-white bg-emerald-600': (this.filter.category === type), 
-      'text-emerald-700 bg-emerald-50 ring-1 ring-emerald-500': (this.filter.category !== type)
-    }
-  }
+      'text-white bg-emerald-600': (this.filter.category === type),
+      'text-emerald-700 bg-emerald-50 ring-1 ring-emerald-500': (this.filter.category !== type),
+    };
+  },
 });
